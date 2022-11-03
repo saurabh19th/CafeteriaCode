e74db9b43387bb6a95f3ece574ac6acf62504293
Agrawal <saurabh-sanjay.agrawal@capgemini.com>
2022-11-03 16:54:01 +0530

    created project

---
+++ src/main/java/com/example/CafeteriaCode/CafeteriaCodeApplication.java
@@ -0,0 +1,13 @@
+package com.example.CafeteriaCode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafeteriaCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CafeteriaCodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CafeteriaCode/CafeteriaCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CafeteriaCode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafeteriaCodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
