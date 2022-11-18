484229ffb5c653e31a2584ecc2ec6b8ced6f9588
Agrawal <saurabh-sanjay.agrawal@capgemini.com>
2022-11-18 15:37:23 +0530

    updated AdminServiceImpl

--- src/main/java/com/example/CafeteriaCode/serviceImpl/CategoryServiceImpl.java
+++ src/main/java/com/example/CafeteriaCode/serviceImpl/CategoryServiceImpl.java
@@ -3,6 +3,7 @@ package com.example.CafeteriaCode.serviceImpl;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 import org.apache.logging.log4j.util.Strings;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,15 +23,21 @@ public class CategoryServiceImpl implements CategoryService {
 	@Autowired
 	CategoryDao categoryDao;
 	
+//  @Autowired
+//  JwtFilter jwtFilter;
+	
 
 	@Override
 	public ResponseEntity<String> addNewCategory(Map<String, String> requestMap) {
 		try {
-			
+//          if(jwtFilter.isAdmin()){
 			if(validateCategoryMap(requestMap,false)) {
 				categoryDao.save(getCategoryFromMap(requestMap, false));
 				return CafeUtils.getResponseEntity("Category Added Successfully",HttpStatus.OK);
 			}
+//          return CafeUtils.getResponseEntity(CafeConstants.INVALID_DATA,HttpStatus.BAD_REQUEST);
+//     }else
+//          return CafeUtils.getResponseEntity(CafeConstants, UNAUTHORIZED_ACCESS, HttpStatus.UNAUTHORIZED);
 
 			
 		}catch(Exception ex) {
@@ -83,7 +90,25 @@ try {
 
 	@Override
 	public ResponseEntity<String> updateCategory(Map<String, String> requestMap) {
-		return null;
-	}
+        try {
+//          if(jwtFilter.isAdmin()){
+		if(validateCategoryMap(requestMap,true)) {
+			Optional optional = categoryDao.findById(Integer.parseInt(requestMap.get("id")));
+			if(!optional.isEmpty()) {
+				categoryDao.save(getCategoryFromMap(requestMap,true));
+				return CafeUtils.getResponseEntity("Category Updated Successfully", HttpStatus.OK);
+				
+			}else {
+				return CafeUtils.getResponseEntity("Category id does not exist", HttpStatus.OK);
+			}
+			}
+		return CafeUtils.getResponseEntity(CafeConstants.INVALID_DATA, HttpStatus.BAD_REQUEST);
+
+        
+    } catch (Exception ex) {
+        ex.printStackTrace();
+    }
+    return CafeUtils.getResponseEntity(CafeConstants.SOMETHING_WENT_WRONG, HttpStatus.INTERNAL_SERVER_ERROR);
+}
 
 }
